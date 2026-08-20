98dac0b12942dbd9c175e760222bc97af2a733ad
Uriah Finkel <Ufinkel@gmail.com>
2026-08-20 21:06:20 +0300

    Add calibration specification

---
+++ src/spec/calibration.ts
@@ -0,0 +1,25 @@
+import { Type, type Static } from "@sinclair/typebox";
+import { AxisSpecSchema, ReferenceLineSpecSchema } from "./common.js";
+
+export const CalibrationDatumSchema = Type.Object({
+  model: Type.String(),
+  population: Type.Optional(Type.String()),
+  horizon: Type.Optional(Type.Number()),
+  predicted: Type.Number({ minimum: 0, maximum: 1 }),
+  observed: Type.Number({ minimum: 0, maximum: 1 }),
+  method: Type.Union([Type.Literal("discrete"), Type.Literal("smooth")]),
+});
+
+export const CalibrationSpecSchema = Type.Object({
+  schemaVersion: Type.Literal("1.0"),
+  type: Type.Literal("calibration"),
+  data: Type.Array(CalibrationDatumSchema),
+  x: Type.Literal("predicted"),
+  y: Type.Literal("observed"),
+  xAxis: AxisSpecSchema,
+  yAxis: AxisSpecSchema,
+  references: Type.Optional(Type.Array(ReferenceLineSpecSchema)),
+});
+
+export type CalibrationDatum = Static<typeof CalibrationDatumSchema>;
+export type CalibrationSpec = Static<typeof CalibrationSpecSchema>;
