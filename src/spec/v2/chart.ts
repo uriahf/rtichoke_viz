ddac2d080e8012f30d1b9ee104fa9f336aa5734e
Uriah Finkel <Ufinkel@gmail.com>
2026-08-21 21:45:48 +0300

    Include gains in the v2 chart union

--- src/spec/v2/chart.ts
+++ src/spec/v2/chart.ts
@@ -1,11 +1,17 @@
 import { Type, type Static } from "@sinclair/typebox";
 import { CalibrationV2SpecSchema } from "./calibration.js";
+import { GainsV2SpecSchema } from "./gains.js";
 import { PrecisionRecallV2SpecSchema } from "./precision_recall.js";
 import { RocV2SpecSchema } from "./roc.js";
 
 /** Canonical v2 union with explicit evaluation and reference ownership. */
 export const RtichokeChartSpecV2Schema = Type.Union(
-  [RocV2SpecSchema, CalibrationV2SpecSchema, PrecisionRecallV2SpecSchema],
+  [
+    RocV2SpecSchema,
+    CalibrationV2SpecSchema,
+    PrecisionRecallV2SpecSchema,
+    GainsV2SpecSchema,
+  ],
   {
     $id: "https://rtichoke.dev/schema/viz/2.0.json",
     title: "rtichoke visualization specification v2",
