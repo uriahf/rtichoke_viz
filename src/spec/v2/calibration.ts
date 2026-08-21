283a65e5d9a43adfe629fa8e6e419f1c195a02bd
Uriah Finkel <Ufinkel@gmail.com>
2026-08-21 18:07:00 +0300

    Add v2 calibration schema

---
+++ src/spec/v2/calibration.ts
@@ -0,0 +1,35 @@
+import { Type, type Static } from "@sinclair/typebox";
+import { BaseChartV2SpecSchema } from "./common.js";
+
+export const CalibrationV2DatumSchema = Type.Object({
+  seriesId: Type.String(),
+  predicted: Type.Number({ minimum: 0, maximum: 1 }),
+  observed: Type.Number({ minimum: 0, maximum: 1 }),
+  method: Type.Union([Type.Literal("discrete"), Type.Literal("smooth")]),
+  events: Type.Optional(Type.Number({ minimum: 0 })),
+  total: Type.Optional(Type.Number({ minimum: 0 })),
+});
+
+export const CalibrationV2DistributionDatumSchema = Type.Object({
+  seriesId: Type.String(),
+  midpoint: Type.Number({ minimum: 0, maximum: 1 }),
+  count: Type.Number({ minimum: 0 }),
+  binWidth: Type.Number({ exclusiveMinimum: 0, maximum: 1 }),
+});
+
+export const CalibrationV2SpecSchema = Type.Intersect([
+  BaseChartV2SpecSchema,
+  Type.Object({
+    type: Type.Literal("calibration"),
+    data: Type.Array(CalibrationV2DatumSchema),
+    distribution: Type.Optional(Type.Array(CalibrationV2DistributionDatumSchema)),
+    x: Type.Literal("predicted"),
+    y: Type.Literal("observed"),
+  }),
+]);
+
+export type CalibrationV2Datum = Static<typeof CalibrationV2DatumSchema>;
+export type CalibrationV2DistributionDatum = Static<
+  typeof CalibrationV2DistributionDatumSchema
+>;
+export type CalibrationV2Spec = Static<typeof CalibrationV2SpecSchema>;
