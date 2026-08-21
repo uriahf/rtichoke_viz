2eaaaba98451220964e4b252af42d3af63d25659
Uriah Finkel <Ufinkel@gmail.com>
2026-08-21 18:08:16 +0300

    Export v2 semantic schemas

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,30 @@ export { RocSpecSchema } from "./spec/roc.js";
 export type { RocDatum, RocSpec } from "./spec/roc.js";
 export { RtichokeChartSpecSchema } from "./spec/chart.js";
 export type { RtichokeChartSpec } from "./spec/chart.js";
+export { RtichokeChartSpecV2Schema } from "./spec/v2/chart.js";
+export type { RtichokeChartSpecV2 } from "./spec/v2/chart.js";
+export {
+  DisplayGroupingSpecSchema,
+  DisplayRoleSchema,
+  EvaluationSpecSchema,
+  ReferenceLineV2SpecSchema,
+  SeriesSpecSchema,
+} from "./spec/v2/common.js";
+export type {
+  DisplayGroupingSpec,
+  DisplayRole,
+  EvaluationSpec,
+  ReferenceLineV2Spec,
+  SeriesSpec,
+} from "./spec/v2/common.js";
+export { RocV2SpecSchema } from "./spec/v2/roc.js";
+export type { RocV2Datum, RocV2Spec } from "./spec/v2/roc.js";
+export { CalibrationV2SpecSchema } from "./spec/v2/calibration.js";
+export type {
+  CalibrationV2Datum,
+  CalibrationV2DistributionDatum,
+  CalibrationV2Spec,
+} from "./spec/v2/calibration.js";
 export {
   rocSpecFromRtichokePython,
   rocSpecFromRtichokeR,
